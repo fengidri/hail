fff9854532db5844d8244ca7479aa0e1a071cd50
fengidri <fengidri@gmail.com>
2015-07-03 17:14:32 +0800

    some change

--- plugin/autofresh.py
+++ plugin/autofresh.py
@@ -20,18 +20,25 @@ class index(handle):
         return self.template('autofresh')
 
 class data(handle):
-    data = "--no data--"
+    data = "-- no data --"
+    tp   = ''
+    index = 0
     def GET(self):
-        return self.data
+        qindex = self.query.get("index")
+        if qindex and qindex == str(self.index):
+            return [self.index, '']
+        data = self.data
+        if self.tp in ['mkiv', 'content']:
+            try:
+                data = texstohtmls(self.data)
+            except:
+                data = "<pre>%s</pre>" % traceback.format_exc()
+        return [self.index, data]
 
     def POST(self):
-        tp = self.forms.get('type')
-        self.data = self.forms.get('data')
-        if tp in ['mkiv', 'content']:
-            try:
-                self.data = texstohtmls(self.data)
-            except:
-                self.data = "<pre>%s</pre>" % traceback.format_exc()
+        self.tp    =  self.forms.get('type')
+        self.data  =  self.forms.get('data')
+        self.index += 1
         return 'OK'
 
 
